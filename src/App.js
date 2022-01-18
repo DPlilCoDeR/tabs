8aff803d6a0b67aa08d01309fcbd62366685f3f9
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-18 19:13:43 +0100

    App fetch json of jobs

--- src/App.js
+++ src/App.js
@@ -3,8 +3,33 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
+
+
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [jobs ,setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const jobs = await response.json();
+    setJobs(jobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <section className='section loading'>
+      <h1>loading</h1>
+    </section>
+  }
+
+  return <main>
+    <h2>{jobs}</h2>
+  </main>
 }
 
-export default App
+export default App;
