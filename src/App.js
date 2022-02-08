0ed709b2f8ccd03ee7d50902ec168350e93229c2
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-02-09 00:01:09 +0100

    show data from state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ const url = 'https://course-api.com/react-tabs-project'
 function App() {
   const [jobs ,setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+  const [value, setValue] = useState(0);
+
+
   const fetchData = async () => {
     const response = await fetch(url);
     const jobs = await response.json();
@@ -26,9 +28,30 @@ function App() {
       <h1>loading</h1>
     </section>
   }
+  const { company, dates, duties, title } = jobs[value]
 
   return <main>
-    <h2>{jobs}</h2>
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
   </main>
 }
 
